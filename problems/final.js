c219e49be40c8e7c165e713920c95da0ad398222
saumittras <saumittra.eee@gmail.com>
2024-09-11 12:24:51 +0600

    Addeed all function on a final file

---
+++ problems/final.js
@@ -0,0 +1,75 @@
+function calculateTax(income, expenses) {
+  if (income < 0 || expenses < 0 || expenses > income) {
+    return "Invalid Input";
+  }
+  const blanceAmount = income - expenses;
+  const totalTax = blanceAmount * 0.2;
+  return totalTax;
+}
+
+function sendNotification(email) {
+  if (
+    typeof email !== "string" ||
+    email.includes("@") !== true ||
+    email.indexOf("@") < 1 ||
+    email.length - 1 === email.indexOf("@")
+  ) {
+    return "Invalid Email";
+  }
+
+  let atSignCount = 0;
+  for (const letter of email) {
+    if (letter === "@") {
+      atSignCount++;
+    }
+  }
+  if (atSignCount > 1) {
+    return "Invalid Email";
+  }
+
+  const emailToArry = email.split("@");
+  const output = emailToArry[0] + " sent you an email from " + emailToArry[1];
+  return output;
+}
+
+function checkDigitsInName(name) {
+  if (typeof name !== "string") {
+    return "Invalid Input";
+  }
+  let leterx;
+  for (const leter of name) {
+    leterx = parseInt(leter);
+    if (isNaN(leterx) === false && typeof leterx === "number") {
+      return true;
+    }
+  }
+  return false;
+}
+
+function calculateFinalScore(obj) {
+  if (obj.constructor !== Object) {
+    return "Invalid Input";
+  }
+
+  let testScore;
+  if (obj.testScore >= 0 && obj.testScore <= 50) {
+    testScore = obj.testScore;
+  }
+
+  let schoolScore;
+  if (obj.schoolGrade >= 0 && obj.schoolGrade <= 30) {
+    schoolScore = obj.schoolGrade;
+  }
+
+  let ppScore = 0;
+  if (obj.isFFamily === true) {
+    ppScore = 20;
+  }
+
+  let totalScore = testScore + schoolScore + ppScore;
+  if (totalScore >= 80) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ problems/problem_05.js
@@ -0,0 +1,1 @@
+function waitingTime(waitingTimes, serialNumber) {}
