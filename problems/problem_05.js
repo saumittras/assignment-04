8985b6772a81dd48354f1bff423bcf15aa5c2272
saumittras <saumittra.eee@gmail.com>
2024-09-11 13:42:18 +0600

    problem 05 solved

--- problems/problem_05.js
+++ problems/problem_05.js
@@ -1,1 +1,20 @@
-function waitingTime(waitingTimes, serialNumber) {}
+function waitingTime(waitingTimes, serialNumber) {
+  const candidateInvervied = waitingTimes.length;
+  if (
+    Array.isArray(waitingTimes) !== true ||
+    typeof serialNumber !== "number" ||
+    candidateInvervied >= serialNumber
+  ) {
+    return "Invalid Input";
+  }
+
+  let totalInterviewingTimes = 0;
+  for (const time of waitingTimes) {
+    totalInterviewingTimes += time;
+  }
+
+  const averageTime = totalInterviewingTimes / candidateInvervied;
+  const result =
+    (serialNumber - 1 - candidateInvervied) * Math.round(averageTime);
+  return result;
+}
